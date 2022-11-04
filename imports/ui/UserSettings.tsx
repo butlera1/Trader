5e58053fcdb57dbd52e64955eb0c3ceb5ea6213c
Arch Butler <spockab@gmail.com>
2022-11-04 10:29:42 -0700

    Revamped the settings tab to compress it and added days of the week.

--- imports/ui/UserSettings.tsx
+++ imports/ui/UserSettings.tsx
@@ -1,21 +1,47 @@
 import React, {useState} from 'react';
-import {Meteor} from "meteor/meteor";
-import {Alert, Button, Col, Input, Popconfirm, Row, Space, Spin} from "antd";
-import { QuestionCircleOutlined } from '@ant-design/icons';
+// @ts-ignore
+import {Meteor} from 'meteor/meteor';
+import {Alert, Button, Col, Input, Popconfirm, Row, Space, Spin} from 'antd';
+import {QuestionCircleOutlined, UserOutlined} from '@ant-design/icons';
 
 let timeoutHandle = null;
 
+
 function UserSettings({userSettings}) {
   const [errorText, setErrorText] = useState(null);
-  
+
+  const Emergency = () => {
+    return (
+      <Space>
+        <Popconfirm
+          title="Are you sure: CLOSE ALL TRADES?"
+          icon={<QuestionCircleOutlined style={{color: 'red'}}/>}
+          onConfirm={onEmergencyClick}
+          okText="Yes"
+          cancelText="No"
+        >
+          <Button
+            style={{marginLeft: 50, float: 'right'}}
+            type="primary"
+            shape="round"
+            size={'large'}
+            danger
+          >
+            Emergency: Close All Trades
+          </Button>
+        </Popconfirm>
+      </Space>
+    );
+  };
+
   const onEmergencyClick = () => {
     Meteor.call('EmergencyCloseAllTrades', (error, result) => {
       if (error) {
         setErrorText(`Failed calling EmergencyCloseAllTrades: ${error.toString()}`);
       }
-    })
+    });
   };
-  
+
   const onChange = (propertyName, value) => {
     // setMap[propertyName](value);
     userSettings[propertyName] = value;
@@ -33,50 +59,32 @@ function UserSettings({userSettings}) {
         }
       });
     }, 3000);
-    
+
   };
-  
+
   return (
     <>
-      <h2>
-        <span style={{fontWeight:'normal', color:'blue'}}>User: </span>{Meteor.user().username}
-        <Popconfirm
-          title="Are you sure: CLOSE ALL TRADES?"
-          icon={<QuestionCircleOutlined style={{color: 'red'}}/>}
-          onConfirm={onEmergencyClick}
-          okText="Yes"
-          cancelText="No"
-        >
-          <Button
-            style={{marginLeft: 50}}
-            type="primary"
-            shape="round"
-            size={'large'}
-            danger
-          >
-            Emergency: Close All Trades
-          </Button>
-        </Popconfirm>
-        <span style={{fontWeight:'normal', fontSize:15, color:'blue', position:'fixed', top:0, right:10}}>Version: 0.1</span>
-      </h2>
-      {errorText
-        ?
-        <Alert
-          message={errorText}
-          style={{marginBottom:2}}
-          type="error"
-          action={
-            <Space>
-              <Button size="small" type="ghost" onClick={() => setErrorText(null)}>
-                Done
-              </Button>
-            </Space>
-          }
-          closable
-        />
-        :
-        <div><br/><br/></div>
-      }
+      <Space>
+        <span style={{fontSize: '32px', color: 'blue'}}><UserOutlined/> {Meteor.user().username}</span>
+        {errorText
+          ?
+          <Alert
+            message={errorText}
+            style={{marginBottom: 2}}
+            type="error"
+            action={
+              <Space>
+                <Button size="small" type="ghost" onClick={() => setErrorText(null)}>
+                  Done
+                </Button>
+              </Space>
+            }
+            closable
+          />
+          :
+          null
+        }
+      </Space>
       {userSettings ?
         <Row style={{paddingBottom: 20}}>
           <Space>
@@ -86,6 +94,7 @@ function UserSettings({userSettings}) {
                         onChange={(e) => onChange('email', e.target.value)}/></Col>
             <Col><Input defaultValue={userSettings.phone} addonBefore={'Phone #'}
                         onChange={(e) => onChange('phone', e.target.value)}/></Col>
+            <Col><Emergency/></Col>
           </Space>
         </Row>
         :
