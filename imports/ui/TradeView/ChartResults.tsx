5a78544036743ca547823b3cc23406058f0b1f52
arch <archb@comcast.net>
2022-12-15 13:30:44 -0700

    Went to 1.2.4 Added filtering for results chart.

--- imports/ui/TradeView/ChartResults.tsx
+++ imports/ui/TradeView/ChartResults.tsx
@@ -3,7 +3,7 @@ import {CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis} from 'recharts';
 // @ts-ignore
 import {useTracker} from 'meteor/react-meteor-data';
 import TradeResults from '../../Collections/TradeResults';
-import {Space} from 'antd';
+import {Select, Space} from 'antd';
 
 interface ISumResults {
   description: string,
@@ -12,53 +12,82 @@ interface ISumResults {
   sum: number,
 }
 
+function getDescription(record) {
+  return record.description ?? `${record.symbol}(${record.quantity ?? 1})`;
+}
+
 function ChartResults() {
-  let wins = 0;
-  let losses = 0;
-  let avgWin = 0;
-  let avgLoss = 0;
-  let maxWin = 0;
-  let maxLoss = 0;
+  const [filterSelections, setFilterSelections] = React.useState([]);
+  const [filterList, setFilterList] = React.useState([]);
+  const [avgLossText, setAvgLossText] = React.useState('0');
+  const [avgWinText, setAvgWinText] = React.useState('0');
+  const [winRate, setWinRate] = React.useState('0');
+  const [lossRate, setLossRate] = React.useState('0');
+  const [wins, setWins] = React.useState(0);
+  const [losses, setLosses] = React.useState(0);
+  const [avgWin, setAvgWin] = React.useState(0);
+  const [avgLoss, setAvgLoss] = React.useState(0);
+  const [maxWin, setMaxWin] = React.useState(0);
+  const [maxLoss, setMaxLoss] = React.useState(0);
 
   const tradeResults: ISumResults[] = useTracker(() => {
     const records: ITradeResults[] = TradeResults.find().fetch();
     const sumResults: ISumResults[] = [];
-    wins = 0.0;
-    losses = 0.0;
-    avgLoss = 0;
-    avgWin = 0;
-    maxWin = 0;
-    maxLoss = 0;
+    let winsTmp = 0.0;
+    let lossesTmp = 0.0;
+    let avgLossTmp = 0;
+    let avgWinTmp = 0;
+    let maxWinTmp = 0;
+    let maxLossTmp = 0;
+    const tempFilterList = [];
     records.reduce((sum, record) => {
-      sum = sum + record.gainLoss;
-      const description = `${record.symbol}(${record.quantity ?? 1}) ${record.whenClosed} ${record.gainLoss.toFixed(2)}`;
-      sumResults.push({
-        description,
-        whenClosed: record.whenClosed,
-        sum,
-        gainLoss: record.gainLoss,
-      });
-      if (record.gainLoss >= 0.0) {
-        wins++;
-        avgWin += record.gainLoss;
-        if (record.gainLoss > maxWin) {
-          maxWin = record.gainLoss.toFixed(2);
-        }
-      } else {
-        losses++;
-        avgLoss += record.gainLoss;
-        if (record.gainLoss < maxLoss) {
-          maxLoss = record.gainLoss.toFixed(2);
+      const description = getDescription(record);
+      if (!tempFilterList.some(item => item.label === description)) {
+        tempFilterList.push({label:description, value: description});
+      }
+      // If record is within the filtering (no filtering then select all).
+      if (filterSelections.length === 0 || filterSelections.includes(description)) {
+        sum = sum + record.gainLoss;
+        sumResults.push({
+          description,
+          whenClosed: record.whenClosed,
+          sum,
+          gainLoss: record.gainLoss,
+        });
+        if (record.gainLoss >= 0.0) {
+          winsTmp++;
+          avgWinTmp += record.gainLoss;
+          if (record.gainLoss > maxWinTmp) {
+            maxWinTmp = record.gainLoss;
+          }
+        } else {
+          lossesTmp++;
+          avgLossTmp += record.gainLoss;
+          if (record.gainLoss < maxLossTmp) {
+            maxLossTmp = record.gainLoss;
+          }
         }
       }
       return sum;
     }, 0.0);
+    setFilterList([...tempFilterList]);
+    setAvgLossText((avgLossTmp / lossesTmp).toFixed(2));
+    setAvgWinText((avgWinTmp / winsTmp).toFixed(2));
+    setWinRate(((winsTmp / sumResults.length) * 100).toFixed(1));
+    setLossRate(((lossesTmp / sumResults.length) * 100).toFixed(1));
+    setWins(winsTmp);
+    setLosses(lossesTmp);
+    setAvgLoss(avgLossTmp);
+    setAvgWin(avgWinTmp);
+    setMaxWin(maxWinTmp);
+    setMaxLoss(maxLossTmp);
     return sumResults;
-  });
-  avgLoss = (avgLoss/losses).toFixed(2);
-  avgWin = (avgWin/wins).toFixed(2);
-  const winRate = ((wins / tradeResults.length) * 100).toFixed(1);
-  const lossRate = ((losses / tradeResults.length) * 100).toFixed(1);
+  }, [filterSelections]);
+
+  const handleFilterChange = (value) => {
+    setFilterSelections([...value]);
+  };
+
   return (
     <Space>
       <LineChart width={600} height={300} data={tradeResults ?? []} margin={{top: 5, right: 20, bottom: 5, left: 0}}>
@@ -71,16 +100,26 @@ function ChartResults() {
       </LineChart>
       <div>
         <Space>
+          <h2>Filter: </h2>
+          <Select
+            mode="multiple"
+            style={{ width: '300px' }}
+            placeholder="To Filter data, select ..."
+            onChange={handleFilterChange}
+            options={filterList}
+          />
+        </Space>
+        <Space>
           <h2>WINS:</h2>
           <h3>{winRate}%</h3>
-          <h3>Avg: ${avgWin}</h3>
-          <h3>Max: ${maxWin}</h3>
+          <h3>Avg: ${avgWinText}</h3>
+          <h3>Max: ${maxWin.toFixed(2)}</h3>
         </Space>
         <Space>
           <h2>Losses:</h2>
           <h3>{lossRate}%</h3>
-          <h3>Avg: ${avgLoss}</h3>
-          <h3>Max: ${maxLoss}</h3>
+          <h3>Avg: ${avgLossText}</h3>
+          <h3>Max: ${maxLoss.toFixed(2)}</h3>
         </Space>
         <Space>
           <h2>Totals:</h2>
